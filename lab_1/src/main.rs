7a77aa5866d75da709a2f60ae93b7d85be0f6205
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-01 19:37:06 +0100

    "third task"

--- lab_1/src/main.rs
+++ lab_1/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
 
     // Zadanie 1
 
-    let year: i64 = 2023;
+    let year: i64 = 2024;
 
     if year % 4 == 0 && year % 100 != 0 || year % 400 == 0 {
         println!("Rok {} jest rokiem przestępnym", year);
@@ -28,5 +28,18 @@ fn main() {
         println!("31 dni");
     }
 
+    // Zadanie 3 i 4
+
+    let celsius: f32 = 21.0;
+    let fahrenheit: f32 = 77.0;
+
+    let celsius_to_fahrenheit: f32 = (celsius * 9.0 / 5.0) + 32.0;
+    let fahrenheit_to_celsius: f32 = (fahrenheit - 32.0) * 5.0 / 9.0;
+    
+    println!("{} stopni Celsjusza to {} stopni Fahrenheita", celsius, celsius_to_fahrenheit);
+    println!("{} stopni Fahrenheita to {} stopni Celsjusza", fahrenheit, fahrenheit_to_celsius);
+
+
+    // Zadanie 5
 
 }
