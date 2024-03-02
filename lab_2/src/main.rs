5ad70e3cf9153a829d6555d3db31f0f9f12c590b
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-02 16:25:11 +0100

    silnia

--- lab_2/src/main.rs
+++ lab_2/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    
+    // Zadanie 1 (Silnia)
+
+    let n = 5;
+    let mut silnia = 1;
+
+    for i in 1..n+1{
+        silnia *= i;
+    }
+
+    println!("Silnia z {} to {}", n, silnia);
 }
