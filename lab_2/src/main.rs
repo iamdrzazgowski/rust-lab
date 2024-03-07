24dc8b1523dd0f346a7b506321a0e90b764ca0ea
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-07 13:06:44 +0100

    lab_2 zadania

--- lab_2/src/main.rs
+++ lab_2/src/main.rs
@@ -1,28 +1,61 @@
-fn main() {
-    
-    // Zadanie 1 (Silnia)
 
-    let n = 5;
+fn silnia(n :usize) -> usize {
     let mut silnia = 1;
 
-    for i in 1..n+1{
+    for i in 1..=n{
         silnia *= i;
     }
 
-    println!("Silnia z {} to {}", n, silnia);
+    return silnia;
+}
 
-    // Zadanie 2 i 3 (Suma cyfr)
+fn suma_cyfr(n: usize) -> usize {
+    let mut num = n;
 
-    let mut num = 134;
     let mut sum = 0;
     
-    while num > 0 {
+    while num != 0 {
         
-        println!("{}", num % 10);
+        // println!("{}", num % 10);
         sum += num % 10;
         num /= 10;
     }
 
-    println!("Suma cyfr to {}", sum);
+    return sum;
+}
+
+fn pitagoras(max_number: usize){
+    for a in 1..=max_number{
+        for b  in a+1..=max_number{
+            for c in b+1..=max_number {
+                if a*a + b*b == c*c{
+                    println!("{}^2 + {}^2 = {}^2", a, b, c);
+                }   
+            }
+        }
+    }
+}
+
+fn main() {
+    
+    
+    println!("Zadanie 1 (Silnia)"); // Zadanie 1 (Silnia)
+
+    let n = 5;
+    println!("Silnia z {} to {} \n", n, silnia(n));
+
+    println!("Zadanie 2 i 3 (Suma cyfr)"); // Zadanie 2 i 3 (Suma cyfr)
+
+    let number = 134;
+
+    println!("Suma cyfr z liczby {} to {} \n", number, suma_cyfr(number));
+
+    // Zadanie 4 (trójki pitagorejskie) 0 < a < b < c.
+
+    println!("Zadanie 4 (trójki pitagorejskie)");
+
+    let max_number = 10;
+
+    pitagoras(max_number);
     
 }
