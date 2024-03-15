1fd7a1cc64f5c89b9b140860664a45d553c0a6c6
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-15 21:04:31 +0100

    lab_3 zadania - przykład z wykorzystaniem referencji

--- lab_3/src/bin/zadanie3.rs
+++ lab_3/src/bin/zadanie3.rs
@@ -1,7 +1,10 @@
-fn collatz_steps(){
-    
+fn collatz_steps(n: &mut i32){
+    *n += 1;
 }
 
 fn main(){
-    let steps = 12;
+    // let steps = 12;
+    let mut current_step = 0;
+    collatz_steps(&mut current_step);
+    println!("Current step: {}", current_step);
 }
