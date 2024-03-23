9593b7f1be840d354456d2077afc971bc57d13dc
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-03-23 15:27:12 +0100

    generator liczb pseudolosowych

--- lab_4/src/bin/zadanie7.rs
+++ lab_4/src/bin/zadanie7.rs
@@ -1,3 +1,21 @@
-fn main(){
-    println!("Hello world");
+fn rand(seed: u64, min: u64, max: u64) -> u64 {
+    let a = 1664525;
+    let c = 1013904223;
+    let m = 2_u64.pow(32);
+
+    let mut x = seed;
+    x = (a * x + c) % m;
+
+    min + x % (max - min + 1)
+}
+
+fn main() {
+    let seed = 45;
+    let min = 0;
+    let max = 1000;
+
+    for _ in 0..10 {
+        let random_number = rand(seed, min, max);
+        println!("{}", random_number);
+    }
 }
