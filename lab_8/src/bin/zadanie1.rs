d9fbf425ace70393f38a0f5755a80d197688adf9
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-05-09 13:46:05 +0200

    lab_8 - zadania

--- lab_8/src/bin/zadanie1.rs
+++ lab_8/src/bin/zadanie1.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Status{
     Running,
     Stopped,
@@ -7,7 +7,7 @@ enum Status{
 
 static mut NEXT_PID: u64 = 0;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Process {
     pid: u64,
     command: String,
@@ -17,13 +17,43 @@ struct Process {
 impl Process {
 
     fn from_command(command: &str) -> Process{
-        let pid = NEXT_PID;
-        NEXT_PID += 1;
+
+        let pid;
+        unsafe{
+            pid = NEXT_PID;
+            NEXT_PID += 1;
+        }
+
         Process{pid, command: command.to_string(), status: Status::Running}
+    }
+
+    fn from_script(script: &str) -> Vec<Process>{
+        // "echo mleko | toilet & ls;"
+        // p1 - echo mleko
+        // p2 - toilet
+        // p3 - ls
+
+        script.to_string().split(|c: char| ['|', '&', ';'].contains(&c)).filter(|s| s.len() > 0).map(|s| Self::from_command(s)).collect()
+
+        // let command: Vec<&str> = script.split(|x| {x == '|' || x == '&' || x == ';'}).collect();
+        // let processes: Vec<Process> = command.iter().map(|x| {Self::from_command(x)}).collect(); 
+        // processes
     }
 
     fn shell() -> Process {
         Self::from_command("sh")
+    }
+
+    fn stop(&mut self){
+        self.status = Status::Stopped;
+    }
+
+    fn start(&mut self){
+        self.status = Status::Running;
+    }
+
+    fn finish(&mut self){
+        self.status = Status::Done;
     }
 }
 
@@ -31,24 +61,25 @@ fn main() {
     let command = "toilet";
     let script = "echo mleko | toilet & ls;";
 
-    let p0 = Process{pid: 0, command: "sh".to_string(), status: Status::Running};
+    // let p0 = Process{pid: 0, command: "sh".to_string(), status: Status::Running};
     let mut p1 = Process::from_command(command);
     let mut p2 = Process::shell();
 
     println!("{:?}", p1);
     println!("{:?}", p2);
-
-    /*
+    
     let p3 = Process::from_script(script);
-    let mut p4 = Process::from_command(command);
+    let p4 = Process::from_command(command);
     p1.stop();
     p2.start();
     p2.finish();
+
     println!("{}", !(p1 == p2));
     println!("{}", !(p1 == p4));
+    
     println!("{}", p3.len() == 3);
     for process in p3 {
         println!("{}: {}", process.pid, process.command);
     }
-    */
+    
 }
