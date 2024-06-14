8106b3de7ebbeeddbb94a73c7deaa272ff442b2d
iamdrzazgowski <iamdrzazgowski@gmail.com>
2024-06-14 11:45:08 +0200

    lab_12 - zadania

--- lab_12/src/main.rs
+++ lab_12/src/main.rs
@@ -1,6 +1,6 @@
-use std::ops::{Mul, MulAssign};
+use std::ops::{Add, Mul, MulAssign};
 
-#[derive(Clone, Eq)]
+#[derive(Debug, Clone, Eq)]
 pub struct Fraction {
     numerator: u32,
     denominator: u32
@@ -58,6 +58,13 @@ impl MulAssign for Fraction {
     }
 }
 
+impl Add for Fraction {
+    type Output = Fraction;
+    fn add(self, other: Fraction) -> Self {
+        self
+    }
+}
+
 // operator ==
 impl PartialEq for Fraction {
     fn eq(&self, other: &Fraction) -> bool {
@@ -72,9 +79,9 @@ impl From<u32> for Fraction {
     }
 }
 
-impl From<(u32,u32)> for Fraction {
-    fn from(value: (u32,u32)) -> Self {
-        Self::new(value.0, value.1)
+impl From<(u32, u32)> for Fraction {
+    fn from(n: (u32, u32)) -> Self {
+        Self::new(n.0, n.1)
     }
 }
 
@@ -88,56 +95,24 @@ impl TryFrom<f32> for Fraction {
         if parts.len() != 2 {
             return Err("NaN".to_string());
         }
-
         let p = parts[1].len() as u32;
-        let d0: u32 = parts[0].to_string().parse::<u32>().unwrap();
-        let d1: u32 = parts[1].to_string().parse::<u32>().unwrap();
-
+        let d0: u32 = parts[0].to_string()
+                              .parse::<u32>()
+                              .unwrap();
+        let d1: u32 = parts[1].to_string()
+                              .parse::<u32>()
+                              .unwrap();
         let f1: Fraction = p.into();
         let f2: Fraction = Fraction::from((d1, u32::pow(10, p)));
-
         Ok(f1 + f2)
-
     }
 }
 
-impl TryFrom<&str> for Fraction {
-    type Error = String;
-
-    fn try_from(a: &str) -> Result<Self, String> {
-        let parts: Vec<&str> = a.split('/').collect();
-        let numerator = parts[0].parse::<u32>().unwrap();
-        let denominator = parts[1].parse::<u32>().unwrap();
-
-        Ok(Fraction::new(numerator, denominator))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn create() {
-        let t = Fraction::new(1, 2);
-        assert_eq!(t.numerator, 1);
-        assert_eq!(t.denominator, 2);
-        assert_eq!(t.as_f64(), 0.5);
-    }
-
-    #[test]
-    fn gcd() {
-        assert_eq!(Fraction::gcd(81, 18), 9);
-        assert_eq!(Fraction::gcd(18, 27), 9);
-    }
-
-    #[test]
-    fn partial_eq() {
-        let mut a = Fraction::new(1, 2);
-        let b = Fraction::new(2, 4);
-        let one = Fraction{numerator:3, denominator:3};
-        assert!(a == b);
-        a *= one;
-        // assert!(a == b);
-    }
+fn main() {
+    let f1 = Fraction::from(7);
+    println!("{:?}", f1);
+    let f1 = Fraction::from((7, 2));
+    println!("{:?}", f1);
+    let f1 = Fraction::try_from(1.5);
+    println!("{:?}", f1);
 }
